1c4eafaf2a110d0641c0b3de6e5d46e6758c8537
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-07-10 14:58:52 -0500

    Tailwind and more

---
+++ tailwind.init.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Adjust the path to your React components
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
